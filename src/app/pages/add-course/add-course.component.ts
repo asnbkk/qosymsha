96a9cbb30929583333c838b16dcfa93610618354
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-05-26 02:55:17 +0600

    hello

--- src/app/pages/add-course/add-course.component.ts
+++ src/app/pages/add-course/add-course.component.ts
@@ -9,54 +9,62 @@ import { TutorService } from 'src/app/services/tutor.service';
   styleUrls: ['./add-course.component.css']
 })
 export class AddCourseComponent implements OnInit {
-  faculty: boolean = false
-  course: boolean = false
-  tutor: boolean = false
+  // faculty: boolean = false
+  // course: boolean = false
+  // tutor: boolean = false
+// 
+  // selectedCardName
+  // selectedCourseName
+    // 
+  // universities = []
+  // faculties = []
+  // courses = []
+  // tutors = []
+  // constructor(
+    // private universityService: UniversityService,
+    // private courseService: CourseService,
+    // private tutorService: TutorService
+    // ) { }
+// 
+  // ngOnInit(): void {
+    // this.getUniversity()
+  // }
+// 
+  // getUniversity() {
+    // this.universityService.getUniversities().subscribe(data => {
+      // this.universities = data
+    // })
+  // }
+// 
+  // onUniversitySelect(name) {
+    // this.faculty = true
+    // let uni = this.universities.find(o => o.name == name)
+    // this.faculties = uni.faculties
+    // console.log(this.faculties)
+  // }
+// 
+  // onFacultySelect(name) {
+    // this.course = true
+    // this.selectedCardName = name
+// 
+    // this.courseService.getCourses(name).subscribe(data => {
+      // this.courses = data
+    // })
+  // }
 
-  selectedCardName
-  selectedCourseName
-    
-  universities = []
-  faculties = []
-  courses = []
-  tutors = []
-  constructor(
-    private universityService: UniversityService,
-    private courseService: CourseService,
-    private tutorService: TutorService
-    ) { }
+  // onCourseSelect(name) {
+  //   this.selectedCourseName = name
+  //   this.tutor = true
+  //   this.tutorService.getByCourse(name).subscribe(data => {
+  //     this.tutors = data
+  //   })
+  // }
+  universities = [
+    {"name": "Казахстанско-Британский технический университет"},
+    {"name": "Назарбаев Университет"}
+  ]
+  
+  ngOnInit() {
 
-  ngOnInit(): void {
-    this.getUniversity()
-  }
-
-  getUniversity() {
-    this.universityService.getUniversities().subscribe(data => {
-      this.universities = data
-    })
-  }
-
-  onUniversitySelect(name) {
-    this.faculty = true
-    let uni = this.universities.find(o => o.name == name)
-    this.faculties = uni.faculties
-    console.log(this.faculties)
-  }
-
-  onFacultySelect(name) {
-    this.course = true
-    this.selectedCardName = name
-
-    this.courseService.getCourses(name).subscribe(data => {
-      this.courses = data
-    })
-  }
-
-  onCourseSelect(name) {
-    this.selectedCourseName = name
-    this.tutor = true
-    this.tutorService.getByCourse(name).subscribe(data => {
-      this.tutors = data
-    })
   }
 } 
